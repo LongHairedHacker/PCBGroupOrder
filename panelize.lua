7f294ababf2cde8f74fb262447e89f9758586974
LongHairedHacker <sebastian@sebastians-site.de>
2015-05-27 14:49:41 +0200

    Build a simpler example

--- panelize.lua
+++ panelize.lua
@@ -7,16 +7,9 @@ local mm = 1e9
 
 local simple = boards.load('./boards/simple/simple')
 
-local simple_extents = extents.compute_board_extents(simple)
-local height = simple_extents.height
-local width = simple_extents.width + 24*mm
-local tabv = panelization.empty_board(10*mm, height)
-local tabh = panelization.empty_board(width, 10*mm)
-
 local layout = {
-	tabh,
-	{ tabv, simple, tabv },
-	tabh,
+	{simple, simple},
+	{simple, simple},
 }
 
 local panel = panelization.panelize(layout, {}, true)
