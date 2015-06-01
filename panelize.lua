01b5d4c4887a7a5e03f6914e19ee8b5e98fbb0a1
LongHairedHacker <sebastian@sebastians-site.de>
2015-06-01 19:26:43 +0200

    Still trying to get round corners to work

--- panelize.lua
+++ panelize.lua
@@ -2,17 +2,17 @@ local boards = require 'boards'
 local extents = require 'boards.extents'
 local manipulation = require 'boards.manipulation'
 local panelization = require 'boards.panelization'
+local extents = require 'boards.extents'
 
 local mm = 1e9
 
 local sss7 = boards.load('./boards/sss7modem/sss7modem')
 
-local layout = {
-	{sss7},
-	{sss7},
-}
+local sss7_extends = extents.compute_board_extents(sss7)
 
-local panel = panelization.panelize(layout, {}, true)
+local filler = panelization.empty_board(10*mm, sss7_extends.height * 2 + 2*mm) 
+
+local panel = panelization.panelize({filler, {sss7, sss7}}, {}, false)
 
 boards.merge_apertures(panel)
 boards.save(panel, './panel/panel')
