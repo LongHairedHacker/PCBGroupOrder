10f3081749c113d5b82044e234d7e867e0353cb6
LongHairedHacker <sebastian@sebastians-site.de>
2015-06-03 11:11:03 +0200

    Revert "Round corners work ... somehow"
    This reverts commit 7b0382681c307c0551802bd64613bfc0dfc4409a.

--- boards/leddriver/leddriver.gbs
+++ boards/leddriver/leddriver.gbs
@@ -1,4 +1,4 @@
-G04 (created by PCBNEW (2013-may-18)-stable) date Mon 01 Jun 2015 11:01:08 PM CEST*
+G04 (created by PCBNEW (2013-may-18)-stable) date Tue 26 May 2015 10:23:06 PM CEST*
 %MOIN*%
 G04 Gerber Fmt 3.4, Leading zero omitted, Abs format*
 %FSLAX34Y34*%
--- panelize.lua
+++ panelize.lua
@@ -7,26 +7,12 @@ local extents = require 'boards.extents'
 local mm = 1e9
 
 local sss7 = boards.load('./boards/sss7modem/sss7modem')
-local driver = boards.load('./boards/leddriver/leddriver')
-
 
 local sss7_extends = extents.compute_board_extents(sss7)
-local driver_extends = extents.compute_board_extents(driver)
 
-local filler = panelization.empty_board(driver_extends.width, sss7_extends.height * 2 - driver_extends.height)
+local filler = panelization.empty_board(10*mm, sss7_extends.height * 2 + 2*mm) 
 
-local panel = panelization.panelize({{sss7, sss7}, {filler, driver}}, {}, false)
+local panel = panelization.panelize({filler, {sss7, sss7}}, {}, false)
 
 boards.merge_apertures(panel)
-
-local panel_extends = extents.compute_board_extents(panel)
-
-print("Current panel width:" .. panel_extends.width / mm .. "mm")
-print("Current panel height:" .. panel_extends.height / mm .. "mm")
-
-local filler_extends = extents.compute_board_extents(filler)
-
-print("Current filler width:" .. filler_extends.width / mm .. "mm")
-print("Current filler height:" .. filler_extends.height / mm .. "mm")
-
 boards.save(panel, './panel/panel')
